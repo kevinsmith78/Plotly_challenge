c8d37013040d2c6daf0326ca197412fd868a24f0
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-29 16:47:04 -0500

    complete overhoaul

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,16 +1,45 @@
-//Use D3 to fetch the data from the JSON file
-d3.json("samples.json").then((importedData) => {
-    //console.log(importedData);
- 
-    //Map the metadata and create by bbtype
-    var bbtype=importedData.metadata.map(types => types.bbtype)
-    console.log('Belly Button: $(bbtype')
+function buildmetadata (id) {
+//Select the element for selecting Data
+    var dropdown =d3.selectAll("#selDataset");
+    //Populate the dropdown menu
+    d3.json("samples.json").then((importedData) => {
+        var subjectIds = importedData.names;
+        subjectIds.forEach((id) => {
+            selector
+            .append("option")
+            .text(id)
+            .property("value",id);
+        });
 
-    //filter sample values by type per assignment
-    var samples = importedData.samples.filter(samp => samp.id.toString() === id)[0];
-    //console.log(samples)
-
-
+    //use the Subject ID from the names to build inital polts
+    const firstSubject = subjectIds[0];
+    updatecharts(firstSubject);
+    updateMetaData(firstSubject);
+    });
 }
 
+function meta(sample) {
+    d3.json("samples.json").then((importedData) => {
+        var metadata=importedData.metadata;
+        var filterArray=metadata.filter(sampleObject => sampleObject.id == sample);
+        var result = filterArray[0]
+        metaPanel.html("");
+        Object.entires(result).forEach(([key,value])=> {
+            metaPanel.append("h6").text('$key.toUpperCase()]: ${value}')
+        })
 
+
+
+    //Create a horizontal bar chart = 
+
+    //Use sample_values as the values for the bar chart.
+    
+    
+    //Use otu_ids as the labels for the bar chart.
+    
+    
+    //Use otu_labels as the hovertext for the chart.
+
+});
+
+}
