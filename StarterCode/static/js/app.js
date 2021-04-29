e808598524ceefc250a4d4a5a36d9f4f56449b92
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-29 18:57:39 -0500

    restart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,66 +1,17 @@
-function buildmetadata(id) {
-    //Select the element for selecting Data
-    var dropdown = d3.selectAll("#selDataset");
-    //Populate the dropdown menu
-    d3.json("samples.json").then((importedData) => {
-        var subjectIds = importedData.names;
-        subjectIds.forEach((id) => {
-            selector
-                .append("option")
-                .text(id)
-                .property("value", id);
-        });
-
-        //use the Subject ID from the names to build inital polts
-        const firstSubject = subjectIds[0];
-        updatecharts(firstSubject);
-        updateMetaData(firstSubject);
-    });
+//1.Use the D3 library to read in samples.json.
+d3.json("samples.json").then(function (data) {
+    //check console
+    console.log(data);
+    var meta = data.metadata[0];
+    console.log(Object.entries(meta));
+    console.log(meta);
 }
+//2.Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+Plotly.newPlot("plot", data, layout);
+//3.Create a bubble chart that displays each sample.
 
-function meta(sample) {
-    d3.json("samples.json").then((importedData) => {
-        var metadata = importedData.metadata;
-        var filterArray = metadata.filter(sampleObject => sampleObject.id == sample);
-        var result = filterArray[0]
-        var metaPanel = d3.select("#sample-metadata");
-        metaPanel.html("");
-        Object.entires(result).forEach(([key, value]) => {
-            metaPanel.append("h6").text('$key.toUpperCase()]: ${value}')
-        });
+//4.Display the sample metadata, i.e., an individual's demographic information.
 
-        //Create a horizontal bar chart 
+//5.Display each key-value pair from the metadata JSON object somewhere on the page.
 
-
-        //Use sample_values as the values for the bar chart.
-
-
-        //Use otu_ids as the labels for the bar chart.
-
-
-        //Use otu_labels as the hovertext for the chart.
-
-    });
-}
-
-function initaldata() {
-    //Select the element for selecting Data
-    var dropdown = d3.selectAll("#selDataset");
-    //Populate the dropdown menu
-    d3.json("samples.json").then((importedData) => {
-        var subjectIds = importedData.names;
-        subjectIds.forEach((id) => {
-            selector
-                .append("option")
-                .text(id)
-                .property("value", id);
-        });
-
-        //use the Subject ID from the names to build inital polts
-        const firstSubject = subjectIds[0];
-        //updatecharts(firstSubject);
-        //updateMetaData(firstSubject);
-    });
-}
-
-initaldata();
+//6.Update all of the plots any time that a new sample is selected.
