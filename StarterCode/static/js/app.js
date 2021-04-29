1d87dfbfb8db0fe251c88acc86e6395a5abeef6a
Kevin Smith <kevin.ar.smith@gmail.com>
2021-04-29 09:55:06 -0500

    data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,11 +1,16 @@
-
-//create the function that retrieves the data from the source
-function buildCharts(sample) {
 //Use D3 to fetch the data from the JSON file
-d3.json("samples.json").then((Data) => {
-   console.log(Data);
-});
-//create the promise
-const dataPromise =d3.json("samples.json");
-console.log("Data Promise:", dataPromise);
+d3.json("samples.json").then((importedData) => {
+    //console.log(importedData);
+ 
+    //Map the metadata and create by bbtype
+    var bbtype=importedData.metadata.map(types => types.bbtype)
+    console.log('Belly Button: $(bbtype')
+
+    //filter sample values by type per assignment
+    var samples = importedData.samples.filter(samp => samp.id.toString() === id)[0];
+    //console.log(samples)
+
+
+}
+
 
