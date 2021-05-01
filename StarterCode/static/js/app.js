0e8e2c3d545423e8088938abf642ab3b2828d164
Kevin Smith <kevin.ar.smith@gmail.com>
2021-05-01 08:07:38 -0500

    values

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,6 @@
 function horizontal(id) {
 //1.Use the D3 library to read in samples.json.
-d3.json("samples.json").then(function (data) {
+d3.json("samples.json").then(data => {
     //check console
     //console.log(data);
     //Collect the values for the upcoming inputs for all of the charts using 0 as start point
@@ -19,6 +19,8 @@ d3.json("samples.json").then(function (data) {
     //console.log('Otu:${ids}')
     //2.c Use otu_labels as the hovertext for the chart.
     var label_otu = data.samples[0].otu_labels.slice(0,10);
+    //Creat the trace
+    var input = [trace];
     //Create the data to develop the trace
     var graph = {
         x: values,
@@ -28,13 +30,16 @@ d3.json("samples.json").then(function (data) {
         color:"green"},
         type:"bar",
         orientation:"h",
-    // create the data placement
-    var data = [trace];
     };
 
-//2.Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-Plotly.newPlot("bar",data, graph);
+//2.Create a horizontal bar chart  
+Plotly.newPlot("bar",input, graph);
+//2.a dropdown menu to display the top 10 OTUs found in that individual.
+    function init() {
+        var choice = d3.select("#selDataset")
+        //look through the data in the original fine
 
+    }
 
 //2b.Create dropdown Menu
 d3.selectAll("#selDataset).on("change",updatePlotly);
@@ -50,7 +55,14 @@ function updatePlotly() {
 //3.Create a bubble chart that displays each sample.
 Plotly.newPlot("bubble", databubble,layout);
     var graph_1 = {
-        x:
+        x:data.samples[0].otu_ids,
+        y:data.samples[0].values,
+        mode: "markers",
+        marker: {
+            size:data.samples[0].values,
+            color:data.samples[0].values,
+        }
+
     }
 
 //4.Display the sample metadata, i.e., an individual's demographic information.
